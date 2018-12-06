fb3ad45659028ef1c85dba011e62f0e721a02daf
Will Newmarch <will@prodpad.com>
2018-12-06 22:24:23 +0000

    Initial commit

---
+++ binary-patterns.js
@@ -0,0 +1,54 @@
+const DATA = [0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0];
+
+/**
+ * Take an array of 0's and 1's and convert to a bitmask.
+ * @param {Array} binary 
+ */
+function getBitmask(binary) {
+    let value = 0;
+    for(let i = 0; i < binary.length; i++) {
+        value += Math.pow(2,i+1) * binary[i];
+    }
+    return value;
+}
+
+/**
+ * Discover patterns in an array of binary.
+ * @param {Array} data 
+ */
+function getPatterns(data,debug=false) {
+
+    let size = data.length;
+    let patterns = [];
+
+    for(let chunk = Math.floor(data.length / 2); chunk > 1; chunk--) {
+        
+        let bitmasks = [];
+        for(let index = 0; (index+chunk-1) < size; index += chunk) {
+            const slice = data.slice(index,index+chunk);
+            bitmasks.push(getBitmask(slice));
+        }
+        
+        let count = 1;
+        let previous = null;
+        for(let i = 0; i <= bitmasks.length; i++) {
+            const current = bitmasks[i] || null;
+            if(current === previous) { 
+                count++;
+            } else {
+                if(count > 2) {
+                    const index = (i-count)*chunk;
+                    const slice = data.slice(index,index+(count*chunk));
+                    patterns.push({chunk,count,index,slice});
+                }
+                count = 1;
+            }
+            previous = current;
+        }
+    }
+
+    return patterns;
+}
+
+console.log('Patterns:');
+console.log(getPatterns(DATA));
